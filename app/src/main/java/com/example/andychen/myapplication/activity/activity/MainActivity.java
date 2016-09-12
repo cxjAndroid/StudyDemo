8f687b3172b4a4322421288577d9212bcfe1de2c
chenxujun <cxj_andorid@163.com>
2016-09-12 18:07:19 +0800

    toolbar

--- app/src/main/java/com/example/andychen/myapplication/activity/activity/MainActivity.java
+++ app/src/main/java/com/example/andychen/myapplication/activity/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.andychen.myapplication.activity.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.widget.SlidingPaneLayout;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,7 +16,6 @@ import com.example.andychen.myapplication.activity.adapter.MenuAdapter;
 import com.example.andychen.myapplication.activity.bean.Doctor;
 import com.example.andychen.myapplication.activity.event.EventMessage;
 import com.example.andychen.myapplication.activity.mvp_model.BaseActivity;
-import com.example.andychen.myapplication.activity.mvp_model.BaseApplication;
 import com.example.andychen.myapplication.activity.mvp_presenter.MainPresenter;
 import com.example.andychen.myapplication.activity.mvp_view.MainView;
 import com.example.andychen.myapplication.activity.utils.IntentUtils;
@@ -42,18 +42,18 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainVie
     SlidingPaneLayout slidingPaneLayout;
     @BindView(R.id.menu_list)
     MyListView menu_list;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     public void initView() {
         setContentView(R.layout.activity_main);
+        setSupportActionBar(toolbar);
     }
 
     @Override
     public void initDate() {
         MobclickAgent.openActivityDurationTrack(false);
-
-        float density = BaseApplication.getApplication().getResources().getDisplayMetrics().density;
-        float densityDpi = BaseApplication.getApplication().getResources().getDisplayMetrics().densityDpi;
 
         showLoadingPage();
         mPresenter.getDoctorsInfo("10");
@@ -97,10 +97,6 @@ public class MainActivity extends BaseActivity<MainPresenter> implements MainVie
             case R.id.btn1:
                 IntentUtils.startActivityLeftIn(this, ThirdActivity.class);
                 EventBus.getDefault().postSticky(new EventMessage<>("from mainPage"));
-                break;
-            case R.id.iv:
-                break;
-            case R.id.mListView:
                 break;
         }
     }
